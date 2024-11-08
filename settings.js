31d2c21d450b2f8e081a170a51ee4f08f3b28480
pandeyg8874 <pushpavish2232@gmail.com>
2024-11-08 08:22:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348165846414"
+global.ownernomer = "919792959103"
 global.ownername = "𝑻𝒐𝒙𝒙𝒊𝒄 𝑩𝒐𝒚"
 global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Nigeria"
 
-global.ownernumber = '2348165846414'  //creator number
+global.ownernumber = '919792959103'  //creator number
 global.ownername = '𝑻𝒐𝒙𝒙𝒊𝒄 𝑻𝒆𝒄𝒉' //owner name
 global.botname = '𝒁𝒆𝒏𝒐𝒏 𝒁𝒐𝒈𝒓𝒂𝒕𝒊𝒔' //name of the bot
 
